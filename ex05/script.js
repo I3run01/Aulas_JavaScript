4168ba788b725dce6c4992d5620ee9b50f6b396f
I3run01 <brunnooa.v@gmail.com>
2022-01-29 09:40:36 -0300

    commit

--- ex05/script.js
+++ ex05/script.js
@@ -2,44 +2,55 @@ var listatxt = [''];
 var listaval = [''];
 
 function adicionar() {
-    var numero = Number(document.querySelector('#numero').value)
+    var numeroS = document.querySelector('#numero').value
+    var numero = Number(numeroS)
     var texto = document.querySelector('#texto')
     var resp = document.querySelector('#resp')
-    var respdiv = document.querySelector('#respdiv')
-    var tam = listatxt.length  
-    listatxt.push(listatxt[tam-1] + `O Valor ${numero} foi adicionado </br>`)
-    listaval.push(numero) 
+    var tam = listatxt.length
 
-    var maior = 0
-    var menor = 0
-    var soma = 0
-    var media = 0
-
-    for(var c = 0; c <= tam; c++ ) {
-        if (listaval[c] < maior) {
-            maior = maior
-        }else {
-            maior = listaval[c]
-        }if(menor == 0) {
-            menor = listaval[c]
-        }else {
-            if (listaval[c] < menor) {
+    if (numero >= 1 && numero <= 100) {
+        listatxt.push(listatxt[tam-1] + `O Valor ${numero} foi adicionado </br>`)
+        listaval.push(numero) 
+    
+        var maior = 0
+        var menor = 0
+        var soma = 0
+        var media = 0
+    
+        texto.innerHTML = listatxt[tam]
+    
+        for(var c = 0; c <= tam; c++ ) {
+            if (listaval[c] < maior) {
+                maior = maior
+            }else {
+                maior = listaval[c]
+            }if(menor == 0) {
                 menor = listaval[c]
             }else {
-                menor = menor
-            }     
-        }soma = Number(soma) + Number(listaval[c])
-    }media = soma/tam
-
-    texto.innerHTML = listatxt[tam]
+                if (listaval[c] < menor) {
+                    menor = listaval[c]
+                }else {
+                    menor = menor
+                }     
+            }soma = Number(soma) + Number(listaval[c])
+        }media = soma/tam
     
-    resp.innerHTML = `
-    Ao todo temos ${tam} números cadastrados. </br></br>
-    O maior número informado foi o ${maior}.</br></br>
-    O menor número informado foi o ${menor}.</br></br>
-    A soma de todos os valores é igual a ${soma}.</br></br>
-    A média de todos os valores é igual a ${media}.
-    `  
+        
+        
+        resp.innerHTML = `
+        Ao todo temos ${tam} números cadastrados. </br></br>
+        O maior número informado foi o ${maior}.</br></br>
+        O menor número informado foi o ${menor}.</br></br>
+        A soma de todos os valores é igual a ${soma}.</br></br>
+        A média de todos os valores é igual a ${media}.
+        `
+
+    }
+
+    else {
+        texto.innerHTML = 'Número inválido!'
+    }
+
 }
 
 function finalizar() {
