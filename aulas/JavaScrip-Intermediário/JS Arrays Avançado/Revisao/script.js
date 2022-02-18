e820017b6878dfd87f9338b70151b2a822d94055
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-02-18 15:28:02 -0300

    commit

---
+++ aulas/JavaScrip-Intermediário/JS Arrays Avançado/Revisao/script.js
@@ -0,0 +1,29 @@
+const nomes = ['Eduardo', 'Maria', 'Joana', 'Joaquina', 'Maria']
+
+//... permite realizar uma cópia do Array
+const novo = [...nomes]
+console.log('copia o Array')
+console.log(novo)
+console.log()
+//delete o último ínidce do Array
+novo.pop()
+console.log('deleta o último item' )
+console.log(novo)
+console.log()
+
+// deleta o primeiro indice do array
+novo.shift
+console.log('deleta o primeiro item')
+console.log(novo)
+console.log()
+
+//coloca um novo indíce no Array
+novo.push('Ricardo')
+console.log('novo indice no Array')
+console.log(novo)
+console.log()
+
+
+
+
+
---
+++ aulas/JavaScrip-Intermediário/JS Arrays Avançado/Revisao/script02.js
@@ -0,0 +1,14 @@
+const nomes = ['Luiz', 'Otávio', 'Miranda']
+const nome = nomes.join(' ')
+
+//união de Arrays
+console.log(nome)
+
+//separação de String para um Array
+const vetor = nome.split(' ')
+console.log(nomes)
+
+
+//Partes de um Array
+const pedacos = vetor.slice(0,2)
+console.log(pedacos)
