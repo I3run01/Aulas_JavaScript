54ef06941046c24e6301a2dbd2d33ba55ea3df38
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-02-23 15:41:20 -0300

    commit

---
+++ aulas/JavaScrip-Intermediário/JS Arrays Avançado/Médoto_Splice/treino02.js
@@ -0,0 +1,7 @@
+// Carlos, Rita, Jéssica => Carlos, Maria, Ricardo, Rita, Jéssica
+
+const pessoas = ['Carlos' , 'Rita', 'Jéssica']
+
+pessoas.splice(1, 0, 'Maria', 'Ricardo')
+
+console.log(pessoas)
