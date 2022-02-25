d3f97053f6e976c50baeb026c2e8a05fed9c49f7
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-02-25 15:22:33 -0300

    commit

---
+++ aulas/JavaScrip-Intermediário/JS Objetos e prototypes/DefineProperty/script02.js
@@ -0,0 +1,26 @@
+//defineProperties
+function Produto(nome, preco, estoque) {
+    this.nome = nome;
+    this.preco = preco
+    this.estoque = estoque
+
+    Object.defineProperties(this, {
+        nome: {
+            enumerable: true,
+            value: nome,
+            writable:true,
+            configurable:true
+        },
+        preco: {
+            enumerable: true,
+            value: nome,
+            writable:true,
+            configurable:true
+        },
+    })
+}
+
+const p1 = new Produto('Camisa', 20, 3)
+p1.estoque = 20
+console.log(p1)
+console.log(Object.keys(p1))
