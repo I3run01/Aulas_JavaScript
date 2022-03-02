a6679a6c891eda76d54839d0f1a7de2d6a67a6f7
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-03-02 11:36:00 -0300

    commit

--- aulas/JavaScrip-Intermediário/Prototypes/Heranças/script01.js
+++ aulas/JavaScrip-Intermediário/Prototypes/Heranças/script01.js
@@ -3,25 +3,36 @@
 Produto.prototype.aumento = function(quantia) {
     this.preco += quantia
 }
-Produto.prototype.desconto = function(quantia) {
-    this.preco -= quantia
-}
 
-
-
-function Produto(nome, preco, cor) {
+function Produto(nome, preco) {
     this.nome = nome
     this.preco = preco
-    this.cor = cor
+
 }
-
-
 
 function Camiseta(nome, preco, cor) {
-    Produto.call(this, nome, preco, cor)
+    Produto.call(this, nome, preco)
+    this.cor = cor
+    
 }
-Camiseta.prototype = Object.create(Produto.prototype)
 
-const camista = new Camiseta('Rgaeta', 7.5, 'Preta')
-camista.aumento(10)
-console.log(camista)
+Camiseta.prototype = Object.create(Produto.prototype)
+Camiseta.prototype.constructor = Camiseta
+
+const camisa01 = new Camiseta('regata', 7.5, 'Preta')
+camisa01.aumento(10)
+console.log(camisa01)
+
+function Caneca(nome, preco, material) {
+    Produto.call(this, nome, preco)
+    this.material = material
+}
+
+Caneca.prototype = Object.create(Produto.prototype)
+Caneca.prototype.constructor = Caneca
+
+const caneca01 = new Caneca('xicara', 1.99, 'vidro')
+caneca01.aumento(10)
+console.log(caneca01)
+
+
