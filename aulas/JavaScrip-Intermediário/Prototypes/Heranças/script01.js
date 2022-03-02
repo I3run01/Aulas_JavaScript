4a6fb51fa4f4b360a81d5bd7462523ab43f7a805
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-03-02 11:16:08 -0300

    commit

--- aulas/JavaScrip-Intermediário/Prototypes/Heranças/script01.js
+++ aulas/JavaScrip-Intermediário/Prototypes/Heranças/script01.js
@@ -1,3 +1,27 @@
 // Produto
 // Camiseta, caneca, lapis
+Produto.prototype.aumento = function(quantia) {
+    this.preco += quantia
+}
+Produto.prototype.desconto = function(quantia) {
+    this.preco -= quantia
+}
 
+
+
+function Produto(nome, preco, cor) {
+    this.nome = nome
+    this.preco = preco
+    this.cor = cor
+}
+
+
+
+function Camiseta(nome, preco, cor) {
+    Produto.call(this, nome, preco, cor)
+}
+Camiseta.prototype = Object.create(Produto.prototype)
+
+const camista = new Camiseta('Rgaeta', 7.5, 'Preta')
+camista.aumento(10)
+console.log(camista)
