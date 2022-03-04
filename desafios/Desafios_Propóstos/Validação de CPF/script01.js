20784bbdb523c6be3f0c92f95697758bf121b4a6
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-03-04 19:35:01 -0300

    commit

--- desafios/Desafios_Propóstos/Validação de CPF/script01.js
+++ desafios/Desafios_Propóstos/Validação de CPF/script01.js
@@ -2,7 +2,7 @@ function termoFunc(string) {
     var lista = string.split('')
 
     var prod = 0
-    var c = 10
+    var c = lista.length + 1
     for (var i of lista ) {
         prod = prod + (Number(i)*c)
         c--
@@ -19,6 +19,9 @@ function termoFunc(string) {
 function Val_CPF(cpf) {
     this.cpf = cpf.replace(/\D+/g, '')
     this.cpf_re2 = cpf.replace(/\D+/g, '').substring(0,9)
+    this.cpf_re1 = cpf.replace(/\D+/g, '').substring(0,10)
+    this.cpf_ut = cpf.replace(/\D+/g, '').substring(9,11)
+    
 }
 
 
@@ -26,9 +29,27 @@ Val_CPF.prototype.indice01 = function() {
     return termoFunc(this.cpf_re2)
 }
 
-var cpf01 = new Val_CPF("705.484.450-52")
+Val_CPF.prototype.indice02 = function() {
+    return termoFunc(this.cpf_re1)
+}
 
-console.log(cpf01.cpf_re2)
-console.log(cpf01.indice01())
+
+
+function validacao(obj) {
+    if (String(obj.indice01()) + String(obj.indice02())  === obj.cpf_ut ) {
+        console.log("CPF válido")
+    }
+    
+    else {
+        console.log("CPF inválido")
+    }
+}
+
+var cpf01 = new Val_CPF("705.484.450-52")
+var cpf02 = new Val_CPF("070.987.720-03")
+
+validacao(cpf01)
+validacao(cpf02)
+
 
 
