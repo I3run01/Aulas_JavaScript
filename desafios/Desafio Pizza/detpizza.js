01e2a66940d27144002e933d80ab7b75cd585567
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-04-18 15:08:31 -0300

    commit

--- desafios/Desafio Pizza/detpizza.js
+++ desafios/Desafio Pizza/detpizza.js
@@ -2,6 +2,10 @@ function detpizza(indice) {
 
     let obj = pizzas[indice]
     document.querySelector('#detpizza').style.display = 'block'
-    document.querySelector('#DetPizzaDiv').innerHTML = obj.nome
+    document.querySelector('#DetPizzaDiv').innerHTML = 
+    `<p><img src="${obj.foto}" alt=""></p>` +
+    `<h1>${obj.nome}</h1>` + 
+    `<p> R$ ${obj.descriao}</p>` +
+    `<p> R$ ${obj.preco}</p>` 
 
 }
